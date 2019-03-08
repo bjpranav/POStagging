b49c09d75e370a43cfd4db422835541da07ec68b
Alagappan <43281916+alagappan28@users.noreply.github.com>
2019-03-08 00:57:53 -0500

    Bigram base model

--- tagger.py
+++ tagger.py
@@ -1,18 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  2 16:57:28 2019
-
-@author: Pranav Krishna
-"""
+#Team GAP
 import nltk
-taggedList=[]
-with open(r"D:\bin\AIT-690\Assignments\Assignment-2\PA2\PA2\pos-train.txt") as file: # Use file to refer to the file object
-   taggedSet = file.read()
-   for line in taggedSet.split():
-       tagTuples=nltk.tag.str2tuple(line)
-       if(tagTuples[1] != None):
-           taggedList.append(nltk.tag.str2tuple(line))
-   
+import sys
+
+#train=sys.argv[1]
+#test=sys.argv[2]
+#key=sys.argv[3]
+
+
+
+#with open(train) as trainset:  # Use file to refer to the file object
+with open(r"C:\Users\alaga\Desktop\sem 2\AIT690\PA2\pos-train.txt") as trainset:
+    tagged_trainset = trainset.read()
+    # removing "[,],\n" which are not needed
+    tagged_trainset=tagged_trainset.replace("[","")
+    tagged_trainset=tagged_trainset.replace("]","")
+    tagged_trainset = tagged_trainset.replace("\n", "")
+
+    taggedList = []
+    only_tags=[]
+    for line in tagged_trainset.split():
+
+        tagTuples = nltk.tag.str2tuple(line)
+        only_tags.append(tagTuples[1])
+        if (tagTuples[1] != None):
+            taggedList.append(nltk.tag.str2tuple(line))
+
+
+
 taggedDict={}
 for x,y in taggedList:
     if(x in taggedDict.keys()):
@@ -20,17 +34,132 @@ for x,y in taggedList:
     else:
         taggedDict[x]=[y]
 
+single_tag_count = {}
+for x,y in taggedList:
+    if(y in single_tag_count.keys()):
+        single_tag_count[y]=single_tag_count[y]+1
+    else:
+        single_tag_count[y]=1
 
-tag='NNP'
-previousTags=[]
-for index,tup in enumerate(taggedList):
-    if(tup[1] == tag):
-        previousTags.append(taggedList[index-1][1])
 
-word='Federal'
-maxProb=0
-for i in set(taggedDict[word]):
-       WordTagprob=taggedDict[word].count(i)/len(taggedDict[word])
-       if(prob>maxProb):
-           tag=i
-           maxProb=prob
+double_tag_count={}
+mer=''
+for i in range(0,len(only_tags)-1):
+    mer=only_tags[i]+' '+only_tags[i+1]
+    if mer in double_tag_count.keys():
+        double_tag_count[mer]=double_tag_count[mer]+1
+    else:
+        double_tag_count[mer]=1
+
+
+
+#with open(test) as testset:
+    #untagged_testset = testset.read()
+    ## TEXT cleaning, removing "[,],\n"
+    #untagged_testset=untagged_testset.replace("[","")
+    #untagged_testset=untagged_testset.replace("]","")
+    #untagged_testset = untagged_testset.replace("\n", "")
+
+untagged_testset="No ,  it  was n't Black Monday . But while  the New York Stock Exchange did n't  fall apart  Friday as  the Dow Jones " \
+  "Industrial Average plunged  190.58 points -- most of  it in  the final hour "
+
+
+
+
+
+#bigram
+bigram=untagged_testset
+tagged_test_string=[]
+
+mer=''
+bigram=bigram.split()
+for i in range(0,len(bigram)):
+    max_prob=[]
+    if bigram[i] in taggedDict:
+        if len(set(taggedDict[bigram[i]]))==1:
+            type=taggedDict[bigram[i]][0]
+        elif i==0:
+            prev='.'
+            curr=bigram[i]
+            if curr in taggedDict:
+                n= len(taggedDict[bigram[i]])
+                unique = list(set(taggedDict[bigram[i]]))
+                for u in unique:
+                    count_val=taggedDict[bigram[i]].count(u)
+                    prob1=count_val/n
+                    mer=prev+' '+u
+                    if mer in double_tag_count.keys():
+                        prob2=double_tag_count[mer]/single_tag_count[u]
+                        total_prob=prob1*prob2
+                        max_prob.append(total_prob)
+                type=unique[max_prob.index(max(max_prob))]
+                prev=type
+        else:
+
+            curr = bigram[i]
+            if curr in taggedDict:
+                n = len(taggedDict[bigram[i]])
+                unique = list(set(taggedDict[bigram[i]]))
+                for u in unique:
+
+                    count_val = taggedDict[bigram[i]].count(u)
+                    prob1 = count_val / n
+                    mer = prev + ' ' + u
+                    if mer in double_tag_count.keys():
+
+                        prob2 = double_tag_count[mer] / single_tag_count[u]
+                        total_prob = prob1 * prob2
+                        max_prob.append(total_prob)
+                        type = unique[max_prob.index(max(max_prob))]
+                    elif bigram[i] in taggedDict:
+
+                        unique = list(set(taggedDict[bigram[i]]))
+
+                        max_val = taggedDict[bigram[i]].count(unique[0])
+
+                        type = unique[0]
+
+                        for u in unique:
+
+                            n = taggedDict[bigram[i]].count(u)
+
+                            if (n > max_val):
+                                max_val = n
+                                type = i
+
+    prev = type
+
+    tagged=bigram[i]+'/'+type
+    tagged_test_string.append(tagged)
+
+
+
+
+tagged_testset_key=r"No/RB ,/, [ it/PRP ][ was/VBD n't/RB Black/NNP Monday/NNP ]./. But/CC while/IN [ the/DT New/NNP York/NNP Stock/NNP Exchange/NNP ]did/VBD n't/RB [ fall/VB ]apart/RB " \
+  r"[ Friday/NNP ]as/IN [ the/DT Dow/NNP Jones/NNP Industrial/NNP Average/NNP ]plunged/VBD [ 190.58/CD points/NNS ]--/: most/JJS of/IN [ it/PRP ]in/IN [ the/DT final/JJ hour/NN "
+
+tagged_testset_key = tagged_testset_key.replace("[", "")
+tagged_testset_key= tagged_testset_key.replace("]", "")
+tagged_testset_key= tagged_testset_key.replace("\n", "")
+
+#with open(key) as testset_key:
+    #tagged_testset_key = testset_key.read()
+    ## TEXT cleaning, removing "[,],\n"
+    #tagged_testset_key = tagged_testset_key .replace("[", "")
+    #tagged_testset_key = tagged_testset_key .replace("]", "")
+    #tagged_testset_key= tagged_testset_key .replace("\n", "")
+
+
+predicted=tagged_test_string
+actual=tagged_testset_key.split()
+
+cnt=0
+for i in range(0,len(predicted)):
+    if predicted[i]==actual[i]:
+        cnt+=1
+
+
+accuracy=(cnt*100)/len(predicted)
+print(accuracy)
+
+
