066b1da3d9854fdd0e0cd09cfe138811bd11ff15
bjpranav <bjpranav@gmail.com>
2019-03-11 02:31:55 -0400

    Update tagger.py

--- tagger.py
+++ tagger.py
@@ -3,6 +3,8 @@ import nltk
 import sys
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 """
 train=sys.argv[1]
 test=sys.argv[2]
@@ -10,24 +12,24 @@ key=sys.argv[3]
 """
 
 
-with open(train) as trainset:  # Use file to refer to the file object
-with open(r"C:\Users\alaga\Desktop\sem 2\AIT690\PA2\pos-train.txt") as trainset:
 
-
+with open(r"D:\bin\AIT-690\Assignments\Assignment-2\PA2\PA2\pos-train.txt") as trainset:
     tagged_trainset = trainset.read()
     # removing "[,],\n" which are not needed
     tagged_trainset=tagged_trainset.replace("[","")
     tagged_trainset=tagged_trainset.replace("]","")
     tagged_trainset = tagged_trainset.replace("\n", "")
-
+    
     only_tags = []
     taggedList = []
     for line in tagged_trainset.split():
-
         tagTuples = nltk.tag.str2tuple(line)
-        only_tags.append(tagTuples[1])
         if (tagTuples[1] != None):
-            taggedList.append(nltk.tag.str2tuple(line))
+            if("|" in list(tagTuples[1])):
+                print("inside")
+                tagTuples=(tagTuples[0],tagTuples[1].split('|')[0])
+            taggedList.append(tagTuples)
+            only_tags.append(tagTuples[1])
 
 
 
@@ -58,8 +60,18 @@ for i in range(0,len(only_tags)-1):
 
 
 
+double_tag_count={}
+mer=''
+for i in range(0,len(only_tags)-1):
+    mer=only_tags[i]+' '+only_tags[i+1]
+    if mer in double_tag_count.keys():
+        double_tag_count[mer]=double_tag_count[mer]+1
+    else:
+        double_tag_count[mer]=1
+
+
 with open(test) as testset:
-with open(r"C:\Users\alaga\Desktop\sem 2\AIT690\PA2\pos-test.txt") as testset:
+with open(r"D:\bin\AIT-690\Assignments\Assignment-2\PA2\PA2\pos-test.txt") as testset:
 
     untagged_testset = testset.read()
     # TEXT cleaning, removing "[,],\n"
@@ -73,12 +85,11 @@ untagged_testset="No ,  it  was n't Black Monday . But while  the New York Stock
 
 
 def extract_tags(tagged_text):
-
     only_tags = []
     for line in tagged_text:
         tagTuples = nltk.tag.str2tuple(line)
-        if "|" in tagTuples[1].split():
-            print(tagTuples[1])
+        if "|" in list(tagTuples[1]):
+            tagTuples=(tagTuples[0],tagTuples[1].split('|')[0])
         only_tags.append(tagTuples[1])
     return only_tags
 
@@ -90,27 +101,26 @@ def tagged_word(prev,curr):
         unique = list(set(taggedDict[bigram[i]]))
         for u in unique:
             count_val = taggedDict[bigram[i]].count(u)
-            prob1 = count_val / n
-
+            prob1 = count_val / single_tag_count[u]
             mer = prev + ' ' + u
             if mer in double_tag_count.keys():
                 prob2 = double_tag_count[mer] / single_tag_count[u]
                 total_prob = prob1 * prob2
                 max_prob.append(total_prob)
-                type = unique[max_prob.index(max(max_prob))]
+                typeVal = unique[max_prob.index(max(max_prob))]
 
             elif bigram[i] in taggedDict:
                 unique = list(set(taggedDict[bigram[i]]))
                 max_val = taggedDict[bigram[i]].count(unique[0])
-                type = unique[0]
+                typeVal = unique[0]
                 for u in unique:
                     n = taggedDict[bigram[i]].count(u)
                     if (n > max_val):
                         max_val = n
-                        type = u
+                        typeVal = u
 
-        prev = type
-        return prev,type
+        prev = typeVal
+        return prev,typeVal
 
 
 #bigram
@@ -125,22 +135,22 @@ for i in range(0,len(bigram)):
     if bigram[i] in taggedDict:
 
         if len(set(taggedDict[bigram[i]]))==1:
-            type=taggedDict[bigram[i]][0]
+            typeVal=taggedDict[bigram[i]][0]
 
         elif i==0:
             prev='.'
             curr=bigram[i]
-            prev,type=tagged_word(prev,curr)
+            prev,typeVal=tagged_word(prev,curr)
 
 
         else:
             curr = bigram[i]
-            prev, type = tagged_word(prev, curr)
+            prev, typeVal = tagged_word(prev, curr)
     else:
-        type="NN"
-        prev=type
+        typeVal="NN"
+        prev=typeVal
 
-    tagged=bigram[i]+'/'+type
+    tagged=bigram[i]+'/'+typeVal
     tagged_test_string.append(tagged)
 
 
@@ -153,7 +163,7 @@ tagged_testset_key=r"No/RB ,/, [ it/PRP ][ was/VBD n't/RB Black/NNP Monday/NNP ]
 """
 
 with open(key) as testset_key:
-with open(r"C:\Users\alaga\Desktop\sem 2\AIT690\PA2\pos-test-key.txt") as testset_key:
+with open(r"D:\bin\AIT-690\Assignments\Assignment-2\PA2\PA2\pos-test-key.txt") as testset_key:
 
     tagged_testset_key = testset_key.read()
     ## TEXT cleaning, removing "[,],\n"
@@ -161,20 +171,29 @@ with open(r"C:\Users\alaga\Desktop\sem 2\AIT690\PA2\pos-test-key.txt") as testse
     tagged_testset_key = tagged_testset_key .replace("]", "")
     tagged_testset_key= tagged_testset_key .replace("\n", "")
 
+tags=list(single_tag_count.keys())
+A = np.zeros((len(tags), len(tags)))
+
+confusion_matrix = pd.DataFrame(A, index=tags, columns=tags)
 
 predicted=extract_tags(tagged_test_string)
 actual=extract_tags(tagged_testset_key.split())
 
 cnt=0
 for i in range(0,len(predicted)):
-    if predicted[i]==actual[i]:
-        cnt+=1
+    if(actual[i] in single_tag_count.keys()):
+        if(predicted[i]==actual[i]):
+            cnt+=1
+            confusion_matrix.at[predicted[i], actual[i]]=confusion_matrix.at[predicted[i], actual[i]] + 1
+        else:
+            confusion_matrix.at[predicted[i], actual[i]]=confusion_matrix.at[predicted[i], actual[i]] + 1
 
 
 accuracy=(cnt*100)/len(predicted)
+
 print(accuracy)
 
 
-confusion_matrix(actual,predicted)
+#confusion_matrix(actual,predicted)
 
 cnf_matrix = confusion_matrix(actual,predicted)
