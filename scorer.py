1d5b7980bf16038975c8843d48456c0074b084b0
Alagappan <43281916+alagappan28@users.noreply.github.com>
2019-03-18 21:33:24 -0400

    18/03/2019

--- scorer.py
+++ scorer.py
@@ -4,16 +4,17 @@
 
 import sys
 import nltk
-import os
 import numpy as np
 import pandas as pd
 
-os.chdir(r'D:\bin\AIT-690\Assignments\Assignment-2')
+
 #accuracy test
 
+
+'''
 predicted=sys.argv[1]
 key=sys.argv[2]
-
+'''
 
 def extract_tags(tagged_text):
     only_tags = []
@@ -25,19 +26,21 @@ def extract_tags(tagged_text):
     return only_tags
 
 
-predicted=open(predicted)
+#predicted=open(predicted)
+predicted = open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStagging\pos-test-with-tags.txt")
 predicted=predicted.read()
 
-tagged_testset_key= open(key)
+#tagged_testset_key= open(key)
+tagged_testset_key = open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStagging\pos-test-key.txt")
 tagged_testset_key=tagged_testset_key.read()
+
 tagged_testset_key = tagged_testset_key .replace("[", "")
 tagged_testset_key = tagged_testset_key .replace("]", "")
 tagged_testset_key= tagged_testset_key .replace("\n", "")
 
-#testset = open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStagging\pos-test-key.txt")
 
 
-#tags=list(single_tag_count.keys())
+
 predicted=extract_tags(predicted.split())
 actual=extract_tags(tagged_testset_key.split())
 
@@ -55,11 +58,10 @@ for i in range(0,len(predicted)):
 
 accuracy=(cnt*100)/len(predicted)
 
-print(accuracy)
-print(confusion_matrix)
-
 with open('pos-tagging-report.txt', 'w') as f:
 #np.savetxt(r'pos-tagging-report.txt', confusion_matrix, fmt='%d',delimiter = "   ")
     f.write("Accuracy: %s" % accuracy+"\n")
     confusion_matrix.to_csv('pos-tagging-report.txt', header=True, index=True, sep='\t', mode='a')
     
+print("pos-tagging-report.txt")
+
--- tagger.py
+++ tagger.py
@@ -1,22 +1,19 @@
 #Team GAP
 import nltk
 import sys
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 from nltk.stem.wordnet import WordNetLemmatizer
+import time
 
 
-"""
-train=sys.argv[1]
-test=sys.argv[2]
-"""
 
+#train=sys.argv[1]
+#test=sys.argv[2]
+
+start = time.time()
 #trainset = open(train)
-trainset = open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStagging\pos-train.txt")
-
+trainset = open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStag\pos-train.txt")
 tagged_trainset = trainset.read()
+
 # removing "[,],\n" which are not needed
 tagged_trainset = tagged_trainset.replace("[", "")
 tagged_trainset = tagged_trainset.replace("]", "")
@@ -28,7 +25,6 @@ for line in tagged_trainset.split():
     tagTuples = nltk.tag.str2tuple(line)
     if (tagTuples[1] != None):
         if ("|" in list(tagTuples[1])):
-
             tagTuples = (tagTuples[0], tagTuples[1].split('|')[0])
         taggedList.append(tagTuples)
         only_tags.append(tagTuples[1])
@@ -41,48 +37,31 @@ for x,y in taggedList:
         taggedDict[x]=[y]
 
 
-single_tag_count = {}
-for x,y in taggedList:
-    if(y in single_tag_count.keys()):
-        single_tag_count[y]=single_tag_count[y]+1
-    else:
-        single_tag_count[y]=1
-
-
 double_tag_count={}
-mer=''
-for i in range(0,len(only_tags)-1):
-    mer=only_tags[i]+' '+only_tags[i+1]
-    if mer in double_tag_count.keys():
-        double_tag_count[mer]=double_tag_count[mer]+1
-    else:
-        double_tag_count[mer]=1
-
+single_tag_count = {}
+for i in range(0,len(only_tags)):
+    flag_single = 0
+    if (only_tags[i] in single_tag_count.keys() and flag_single == 0):
+        single_tag_count[only_tags[i]] = single_tag_count[only_tags[i]] + 1
+        flag_single = 1
+    elif(flag_single == 0):
+        single_tag_count[only_tags[i]] = 1
+    if(i<len(only_tags)-1):
+        mer = only_tags[i] + ' ' + only_tags[i + 1]
+        if mer in double_tag_count.keys():
+            double_tag_count[mer] = double_tag_count[mer] + 1
+        else:
+            double_tag_count[mer] = 1
 
 
 #testset = open(test)
 testset=open(r"C:\Users\alaga\Desktop\sem 2\AIT690\POStagging\pos-test.txt")
-
 untagged_testset = testset.read()
+
 # TEXT cleaning, removing "[,],\n"
 untagged_testset = untagged_testset.replace("[", "")
 untagged_testset = untagged_testset.replace("]", "")
 untagged_testset = untagged_testset.replace("\n", "")
-
-
-""""
-untagged_testset="No ,  it  was n't Black Monday . But while  the New York Stock Exchange did n't  fall apart  Friday as  the Dow Jones Industrial Average plunged  190.58 points -- most of  it in  the final hour "
-"""
-
-
-def extract_tags(tagged_text):
-    only_tags = []
-    for line in tagged_text:
-        tagTuples = nltk.tag.str2tuple(line)
-        if "|" in list(tagTuples[1]):
-            tagTuples=(tagTuples[0],tagTuples[1].split('|')[0])
-        only_tags.append(tagTuples[1])
-    return only_tags
 
 
 def tagged_word(prev,curr):
@@ -115,7 +94,7 @@ def tagged_word(prev,curr):
         return prev,typeVal
 
 
-COUNT=0
+
 bigram=untagged_testset
 tagged_test_string=[]
 tagged=''
@@ -128,7 +107,6 @@ for i in range(0,len(bigram)):
 
         if len(set(taggedDict[bigram[i]]))==1:
             typeVal=taggedDict[bigram[i]][0]
-            COUNT=COUNT+1
 
         elif i==0:
             prev='.'
@@ -140,17 +118,22 @@ for i in range(0,len(bigram)):
             curr = bigram[i]
             prev, typeVal = tagged_word(prev, curr)
     else:
-        typeVal="NN"
+        if bigram[i][0].isdigit()==True:
+            typeVal = "CD"
+        elif bigram[i][-1]=='s':
+            typeVal = "NNS"
+        elif bigram[i][-2:]=='ed':
+            typeVal = "VBN"
+        elif bigram[i][-4:] == 'able':
+            typeVal = "JJ"
+        elif bigram[i][0].isupper()==True:
+            typeVal = "NNP"
+        else:
+            typeVal = "NNP"
         prev=typeVal
 
     tagged=bigram[i]+'/'+typeVal
     tagged_test_string.append(tagged)
-
-firstTag=tagged_test_string[0]
-prev = nltk.tag.str2tuple(firstTag)[1]
-Lem = WordNetLemmatizer()
-
-#nltk.download('wordnet')
 
 for i in range(0,(len(tagged_test_string)-2)):
     currTag=nltk.tag.str2tuple(tagged_test_string[i])[1]
@@ -180,12 +163,12 @@ for i in range(0,(len(tagged_test_string)-2)):
         tagged_test_string[i]=tagged
 
 
-
-
 with open('pos-test-with-tags.txt', 'w') as f:
     for item in tagged_test_string:
         f.write("%s\n" % item)
 
-
-
+end = time.time()
+runtime = end-start
+print("Run Time: ",runtime)
+print("pos-test-with-tags.txt")
 
